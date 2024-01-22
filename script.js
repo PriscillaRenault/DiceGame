d2462fc0113ad4538c610c663012da07422c168b
PriscillaRenault <priscilla.renault72460@gmail.com>
2024-01-22 15:42:51 +0100

    current ok, global ok, dot ok, bug reset current after hold

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 /*
-1
-2 créer une nombre aléatoire entre 1 et 6 etl'affcher le nombre aléatoire sur le dé
-3 ajouter ce nombre aléatoire au current
-4 si 1 remettre le total aléatoire à 0 et passez au second joueur
-5 si hold ajouter le current au global et passez au second joueur
+
+créer une nombre aléatoire entre 1 et 6 etl'affcher le nombre aléatoire sur le dé
+ajouter ce nombre aléatoire au current
+si 1 remettre le total aléatoire à 0 et passez au second joueur
+si hold ajouter le current au global et passez au second joueur
+Si newGame remettre tous les scores à 0
+Ajouter une bullet au joueur active
 */
 
 const diceElements = document.getElementsByClassName('js-dice')
 
-let currentValue = document.getElementById('js-currentPlayer1')
-let globalValue = document.getElementById('js-globalPlayer1')
-let currentScore = currentValue
-let globalScore = globalValue
-
-//random dice value
-function diceValue() {
-  return Math.floor(Math.random() * 6) + 1
-}
-//hide other dices
-const matchingValue = diceValue().toString()
-console.log(matchingValue)
+let currentElement = document.getElementById('js-currentPlayer1')
+let globalElement = document.getElementById('js-globalPlayer1')
+let dot = document.getElementById('js-dotPlayer1')
+let currentPlayerScore = 0
+let globalPlayerScore = 0
+let roll = document.getElementById('js-roll')
+let hold = document.getElementById('js-hold')
 
 function rollDice() {
+  //random dice value
+  function diceValue() {
+    return Math.floor(Math.random() * 6) + 1
+  }
+  //hide other dices
+  const matchingValue = diceValue().toString()
+  console.log(matchingValue)
   //show the dice of diceValue
   for (let i = 0; i < diceElements.length; i++) {
     const element = diceElements[i]
@@ -31,38 +35,56 @@ function rollDice() {
       element.classList.remove('hidden')
     }
   }
+  //add to current if diceNumber!==1 else currentPlayerScore=0 and change gamer
   const diceNumber = parseInt(matchingValue)
   if (diceNumber !== 1) {
-    currentScore += diceNumber
-    currentValue.textContent = currentScore
+    currentPlayerScore += diceNumber
+    console.log(currentPlayerScore)
+    currentElement.textContent = currentPlayerScore
   } else {
-    currentScore = 0
-    currentValue.textContent = 0
+    currentPlayerScore = 0
+    currentElement.textContent = 0
     switchPlayer()
   }
 }
-rollDice()
 
+//Save in  GlobalPlayer score
+function holdScore() {
+  globalPlayerScore += currentPlayerScore
+  globalElement.textContent = globalPlayerScore
+  currentPlayerScore = 0
+  switchPlayer()
+}
+
+const player1 = 'player1'
+const player2 = 'player2'
+let activePlayer = player1
 //switch between the 2 players
 function switchPlayer() {
-  if (active === player1) {
-    active === player2
+  if (activePlayer === player1) {
+    activePlayer = player2
   } else {
-    active === player1
+    activePlayer = player1
   }
-  switch (active) {
+  switch (activePlayer) {
     case 'player1': {
-      currentValue = document.getElementById('js-currentPlayer1')
-      globalValue = document.getElementById('js-globalPlayer1')
-      currentScore = currentValue
-      globalScore = globalValue
+      currentPlayerScore = 0
+      dot.classList.add('hidden')
+      currentElement = document.getElementById('js-currentPlayer1')
+      globalElement = document.getElementById('js-globalPlayer1')
+      dot = document.getElementById('js-dotPlayer1')
+      dot.classList.remove('hidden')
+
       break
     }
     case 'player2': {
-      currentValue = document.getElementById('js-currentPlayer2')
-      globalValue = document.getElementById('js-globalPlayer2')
-      currentScore = currentValue
-      globalScore = globalValue
+      currentPlayerScore = 0
+      dot.classList.add('hidden')
+      currentElement = document.getElementById('js-currentPlayer2')
+      globalElement = document.getElementById('js-globalPlayer2')
+      dot = document.getElementById('js-dotPlayer2')
+      dot.classList.remove('hidden')
+
       break
     }
     default: {
@@ -70,3 +92,5 @@ function switchPlayer() {
     }
   }
 }
+roll.addEventListener('click', rollDice)
+hold.addEventListener('click', holdScore)
