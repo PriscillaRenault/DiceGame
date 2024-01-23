92b8d26bcc327cfb2cecd17b7424b4ede7a1c410
PriscillaRenault <priscilla.renault72460@gmail.com>
2024-01-23 16:04:45 +0100

    create bs modal to show Winner

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 /*
-créer une nombre aléatoire entre 1 et 6 etl'affcher le nombre aléatoire sur le dé
-ajouter ce nombre aléatoire au current
-si 1 remettre le total aléatoire à 0 et passez au second joueur
-si hold ajouter le current au global et passez au second joueur
-Si newGame remettre tous les scores à 0
+Create a random number and show it on the dice
+Add to current except if it's one => current = 0 and switch player 
+Onclick hold add current to global and switch player
 Ajouter une bullet au joueur active
-créer une modale activePlayer Win
-Créer une fin de jeu
+End game at 100 and show Winner modal
+Onclick newGame Erase all the scores
+
 */
 
 const diceElements = document.getElementsByClassName('js-dice')
@@ -16,9 +15,13 @@ let globalElement = document.getElementById('js-globalPlayer1')
 let dot = document.getElementById('js-dotPlayer1')
 let currentPlayerScore = 0
 let globalPlayerScore = 0
+let player = 'Player 1'
 const roll = document.getElementById('js-roll')
 const hold = document.getElementById('js-hold')
 const game = document.getElementById('js-newGame')
+const winnerModal = document.getElementById('winner')
+const myInput = document.getElementById('input')
+let textWinner = document.getElementById('textWinner')
 
 function rollDice() {
   //random dice value
@@ -27,7 +30,7 @@ function rollDice() {
   }
   //hide other dices
   const matchingValue = diceValue().toString()
-  console.log(matchingValue)
+
   //show the dice of diceValue
   for (let i = 0; i < diceElements.length; i++) {
     const element = diceElements[i]
@@ -37,11 +40,10 @@ function rollDice() {
       element.classList.remove('hidden')
     }
   }
-  //add to current if diceNumber!==1 else currentPlayerScore=0 and change gamer
+  //add to current if diceNumber!==1 rrentPlayerScore=0 and change gamer
   const diceNumber = parseInt(matchingValue)
   if (diceNumber !== 1) {
     currentPlayerScore += diceNumber
-    console.log(currentPlayerScore)
     currentElement.textContent = currentPlayerScore
   } else {
     currentPlayerScore = 0
@@ -50,14 +52,21 @@ function rollDice() {
   }
 }
 
-//Save in  GlobalPlayer score
+//Save in  GlobalPlayer score and show Winner or switchPlayer
 function holdScore() {
   globalPlayerScore += currentPlayerScore
   globalElement.textContent = globalPlayerScore
-  currentPlayerScore = 0
-  switchPlayer()
+  if (globalPlayerScore >= 20) {
+    winnerModal.addEventListener('shown.bs.modal', () => {
+      myInput.focus()
+    })
+    textWinner.textContent = `Le vainqueur est le ${player} `
+  } else {
+    switchPlayer()
+  }
 }
 
+// New game function
 function newGame() {
   newScores = document.querySelectorAll('.js-newGame')
   newScores.forEach((element) => {
@@ -65,12 +74,13 @@ function newGame() {
   })
 }
 
+//switch between the 2 players
 const player1 = 'player1'
 const player2 = 'player2'
 let activePlayer = player1
-//switch between the 2 players
 function switchPlayer() {
   currentPlayerScore = 0
+
   if (activePlayer === player1) {
     activePlayer = player2
   } else {
@@ -78,20 +88,31 @@ function switchPlayer() {
   }
   switch (activePlayer) {
     case 'player1': {
-      dot.classList.add('hidden')
+      //Assignment variable to player 1
       currentElement.textContent = 0
       currentElement = document.getElementById('js-currentPlayer1')
       globalElement = document.getElementById('js-globalPlayer1')
+      globalPlayerScore = parseInt(globalElement.textContent)
+
+      // Delete dot to player 2 and Assignment to player 1
+      dot.classList.add('hidden')
       dot = document.getElementById('js-dotPlayer1')
       dot.classList.remove('hidden')
+
+      player = 'Joueur 1'
 
       break
     }
     case 'player2': {
-      dot.classList.add('hidden')
+      //Assignment variable to player 2
       currentElement.textContent = 0
       currentElement = document.getElementById('js-currentPlayer2')
       globalElement = document.getElementById('js-globalPlayer2')
+      globalPlayerScore = parseInt(globalElement.textContent)
+      player = 'Player 2'
+
+      // Delete dot to player 1 and Assignment to player 2
+      dot.classList.add('hidden')
       dot = document.getElementById('js-dotPlayer2')
       dot.classList.remove('hidden')
 
